fc712bfec57952600047703f026535dde0d8caa0
Cristiano Miranda <cmoliveira2@latam.stefanini.com>
2018-04-23 14:07:51 -0300

    chat funcional

--- src/TrataRequest.java
+++ src/TrataRequest.java
@@ -2,9 +2,7 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-//TRATA MENSAGENS ENVIADAS DOS CLIENTES E REENVIA
 public class TrataRequest extends Thread {
-
 	private Socket socket;
 	private Scanner entrada;
 	
@@ -12,13 +10,10 @@ public class TrataRequest extends Thread {
 		this.socket = socket;
 		this.entrada = new Scanner(this.socket.getInputStream());
 		Servidor.listaClientes.add(this.socket.getOutputStream());
-		
-		
 	}
 	
 	public void run(){
 		do {
-			
 			String linha = entrada.nextLine();
 			if(linha!=null&&!linha.isEmpty()) {
 				String[] fields = linha.split(";");
@@ -26,19 +21,11 @@ public class TrataRequest extends Thread {
 				String msg = fields[1];
 				System.out.println("Servidor recebeu uma mensagem de : "+user);
 				
-				/*
-				//efetua broadcast da mensagem para todos os clientes
-				Servidor.listaClientes.forEach(saidaDaLista->{
-					saidaDaLista.saida.println(user+" "+msg);
-				});
-				*/
 				for(OutputStream c : Servidor.listaClientes) {
-					PrintWriter saida = new PrintWriter(c);
+					PrintWriter saida = new PrintWriter(c);//printstream x printwriter dados e texto
 					saida.println(user+" "+msg);
 					saida.flush();
 				}
-				
-				
 			}
 		}while(true);
 	}
